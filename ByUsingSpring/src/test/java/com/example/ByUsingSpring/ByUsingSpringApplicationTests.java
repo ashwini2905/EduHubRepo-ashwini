30ae858bd0f8d0f708e9484d379a84a4ddd5293f
ashwini2905 <142107527+ashwini2905@users.noreply.github.com>
2023-08-24 16:50:44 +0530

    Add files via upload

---
+++ ByUsingSpring/src/test/java/com/example/ByUsingSpring/ByUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ByUsingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByUsingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
